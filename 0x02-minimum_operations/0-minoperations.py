28591d20137a1c89fdcd41b98d001012185e01f0
kevkatam <kevkatam@gmail.com>
2024-03-19 17:03:45 +0300

    modified 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -8,14 +8,17 @@ def minOperations(n):
     """ function that calculates the fewest number of operations needed
     to result in exactly n H characters in the file.
     """
-    arr = [1, 2]
-    if n % 3 == 0:
-        for i in range(3, n+1, 3):
+    if not n:
+        return 0
+    arr = []
+    while n % 2 == 0:
+        arr.append(2)
+        n //= 2
+
+    for i in range(3, int(n**0.5) + 1, 2):
+        while n % i == 0:
             arr.append(i)
-    elif n % 2 == 0:
-        for i in range(3, n+1, 2):
-            arr.append(i)
-    else:
-        for i in range(3, n+1, 1):
-            arr.append(i)
-    return len(arr) + 1
+            n //= i
+    if n > 2:
+        arr.append(n)
+    return sum(arr)
