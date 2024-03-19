87d8c7603c0be2e0648663a2aceac582789692d9
kevkatam <kevkatam@gmail.com>
2024-03-19 16:39:06 +0300

    adding 0-minoperations.py

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+module containing function that is solution to minimum operations problem
+"""
+
+
+def minOperations(n):
+    """ function that calculates the fewest number of operations needed
+    to result in exactly n H characters in the file.
+    """
+    arr = [1, 2]
+    if n % 3 == 0:
+        for i in range(3, n+1, 3):
+            arr.append(i)
+    elif n % 2 == 0:
+        for i in range(3, n+1, 2):
+            arr.append(i)
+    else:
+        for i in range(3, n+1, 1):
+            arr.append(i)
+    return len(arr) + 1
---
+++ 0x02-minimum_operations/README.md
@@ -0,0 +1,5 @@
+# Minimum Operations
+
+Project done during alx fullstack software engineering programme
+
+Aim is to prepare for technical interview
