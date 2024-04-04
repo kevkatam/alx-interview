d8fc8726250ace6f87d606405ac968f6358187dc
kevkatam <kevkatam@gmail.com>
2024-04-04 15:34:33 +0300

    modified 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,6 +7,10 @@ validUTF8 module
 def validUTF8(data):
     """ determines if a given data set represents a valid UTF-8 encoding """
     for i in data:
-        if i > 255:
+        j = bin(i)[2:]
+        if len(j) > 8:
             return False
+
+    if len(j) >= 2 and j[:2] != '10':
+        return False
     return True
