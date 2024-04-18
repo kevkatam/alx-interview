970f0098d0c52801f9416d22486e58ce17950f85
kevkatam <kevkatam@gmail.com>
2024-04-18 11:48:44 +0300

    modified 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,23 +2,22 @@
 
 const movieid = process.argv[2];
 
-const superagent = require('superagent');
+const request = require('request');
 
-async function getfilms () {
-  try {
-    const res = await superagent.get(`https://swapi-api.alx-tools.com/api/films/${movieid}`);
-    if (res.body && res.body.characters) {
-      const characters = res.body.characters;
-      for (const character of characters) {
-        const result = await superagent.get(character);
-        if (result.body && result.body.name) {
-          const name = result.body.name;
+const url = `https://swapi-api.alx-tools.com/api/films/${movieid}`;
+
+request(url, function (error, response, body) {
+  if (error == null) {
+    const res = JSON.parse(body);
+    const characters = res.characters;
+    for (const character of characters) {
+      request(character, function (error, response, body) {
+        if (error == null) {
+          const result = JSON.parse(body);
+          const name = result.name;
           console.log(name);
         }
-      }
+      });
     }
-  } catch (err) {
-    console.error(err);
   }
-}
-getfilms();
+});
