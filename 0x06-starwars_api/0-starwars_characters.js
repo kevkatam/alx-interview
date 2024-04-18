3b2eb8efd36c362bd125ca02831ab1c8076739dd
kevkatam <kevkatam@gmail.com>
2024-04-18 22:41:33 +0300

    modified 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -10,14 +10,19 @@ request(url, function (error, response, body) {
   if (error == null) {
     const res = JSON.parse(body);
     const characters = res.characters;
-    for (const character of characters) {
-      request(character, function (error, response, body) {
-        if (error == null) {
-          const result = JSON.parse(body);
-          const name = result.name;
-          console.log(name);
-        }
-      });
-    }
+    getNames(characters, 0);
   }
 });
+
+function getNames (characters, idx) {
+  request(characters[idx], function (error, response, body) {
+    if (error == null) {
+      const res = JSON.parse(body);
+      const name = res.name;
+      console.log(name);
+      if (idx < characters.length - 1) {
+        getNames(characters, idx + 1);
+      }
+    }
+  });
+}
